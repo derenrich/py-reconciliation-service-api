6fd1461a5bf765e6d4a6d2f93a6fdfbcbd406d15
Daniel Erenrich <daniel@erenrich.net>
2024-01-10 17:34:35 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Python Reconciliation Service API
+=================================
+
+This is a python implementation of the [Reconciliation Service API v0.2](https://w3c.github.io/cg-reports/reconciliation/CG-FINAL-specs-0.2-20230410/).
---
+++ py_reconciliation_service_api/models/manifest.py
@@ -0,0 +1,47 @@
+from typing import Any, Dict, List, Type, Union, cast, Optional
+
+from . import BaseSchema
+
+
+class UrlView(BaseSchema):
+    url: str
+
+
+class ManifestType(BaseSchema):
+    id: str
+    name: str
+
+
+class PreviewManifest(BaseSchema):
+    url: str
+    width: int
+    height: int
+
+
+class SuggestServiceManifestEntry(BaseSchema):
+    service_url: Optional[str] = None
+    service_path: Optional[str] = None
+    flyout_service_url: Optional[str] = None
+    flyout_service_path: Optional[str] = None
+
+
+class SuggestServiceManifest(BaseSchema):
+    entity: Optional[SuggestServiceManifestEntry] = None
+    property: Optional[SuggestServiceManifestEntry] = None
+    type: Optional[SuggestServiceManifestEntry] = None
+
+
+class Manifest(BaseSchema):
+    versions: List[str]
+    name: str
+    identifier_space: str
+    schema_space: str
+    documentation: Optional[str] = None
+    service_version: Optional[str] = None
+    logo: Optional[str] = None
+    view: Optional[UrlView] = None
+    feature_view: Optional[UrlView] = None
+    default_types: Optional[List[ManifestType]] = None
+    preview: Optional[PreviewManifest] = None
+    suggest: Optional[SuggestServiceManifest] = None
+    batch_size: Optional[int] = None
